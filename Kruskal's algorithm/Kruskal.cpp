342e7647cc6725d46164a58fc675917a32577442
Creator227 <makhnitskiy1@yandex.by>
2019-11-05 09:10:36 +0300

    C++ algos

---
+++ Kruskal's algorithm/Kruskal.cpp
@@ -0,0 +1,51 @@
+﻿
+#include "pch.h"
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	ifstream fin("graph.in");
+	ofstream fout("graph.out");
+	int n, m;
+	fin >> n >> m;
+	//Вес ребра, начало ребра - конец ребра
+	vector < pair < int, pair<int, int> > > g(m); 
+	for(int i = 0; i < m; i++)
+	{
+
+		int weight, n1, n2;
+		fin >> weight >> n1 >> n2;
+		n1--;
+		n2--;
+		g[i].first = weight;
+		g[i].second = make_pair(n1, n2);
+	}
+	int cost = 0;
+	vector < pair<int, int> > core;
+
+	sort(g.begin(), g.end());
+	vector<int> tree_id(n);
+	for (int i = 0; i < n; ++i)
+		tree_id[i] = i;
+	for (int i = 0; i < m; ++i)
+	{
+		int a = g[i].second.first, b = g[i].second.second, l = g[i].first;
+		if (tree_id[a] != tree_id[b])
+		{
+			cost += l;
+			core.emplace_back(a, b);
+			int old_id = tree_id[b], new_id = tree_id[a];
+			for (int j = 0; j < n; ++j)
+				if (tree_id[j] == old_id)
+					tree_id[j] = new_id;
+		}
+	}
+	for(auto i = core.begin(); i != core.end(); ++i)
+	{
+		fout << i->first + 1 << " " << i->second + 1 << endl;
+	}
+
+	return 0;
+}
---
+++ Maze/Maze.cpp
@@ -0,0 +1,135 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+#include <list>
+#include <stack>
+#include <fstream>
+using namespace std;
+int get_num(pair<int, int> room, int cols)
+{
+	int k = 0;
+	k = (room.first - 1) * cols + room.second - 1;
+	
+	return k;
+}
+pair<int, int> get_pair(const int k, int cols)
+{
+	pair<int, int> p;
+	p.first = (k / cols) + 1;
+	p.second = (k % cols) + 1;
+	return p;
+}
+int main()
+{
+	ifstream fin("maze.in");
+	ofstream fout("maze.out");
+
+	int rows, cols, x_start, y_start, x_finish, y_finish;
+	fin >> rows >> cols >> x_start >> y_start >> x_finish >> y_finish;
+	bool ** maze = new bool*[rows];
+	for (int i = 0; i < rows; i++)
+		maze[i] = new bool[cols];
+
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			fin >> maze[i][j];
+
+	list<pair<int, int>> que;
+	que.push_back(make_pair(x_start, y_start));
+
+	if (x_start == x_finish && y_start == y_finish)
+	{
+		fout << 0 << endl;
+		fout << x_start << " " << y_start << endl;
+		return 0;
+	} 
+	
+	int *parents = new int[cols * rows];
+	bool find = false;
+	while (!que.empty())
+	{
+		pair<int, int> room = que.front();
+		que.pop_front();
+		if (room.first == x_finish && room.second == y_finish)
+		{
+			find = true;
+			break;
+		}
+
+		int x = room.first - 1;
+		int y = room.second - 1;
+		
+
+		//Комнаты, которые можно проверять, не выходя за пределы лабиринта
+		bool left, up, right, down;
+		left = up = right = down = true;
+		if (y == 0)
+			left = false;
+		if (x == 0)
+			up = false;
+		if (y == cols - 1)
+			right = false;
+		if (x == rows - 1)
+			down = false;
+
+		//Проверка комнат
+		if (left && maze[x][y - 1])
+		{
+			parents[x * cols + y - 1] = get_num(room, cols);
+			maze[x][y - 1] = false;
+			que.push_back(make_pair(x + 1, y));
+		}
+		if (up && maze[x - 1][y])
+		{
+			
+			parents[(x - 1)*cols + y] = get_num(room, cols);
+			maze[x - 1][y] = false;
+			que.push_back(make_pair(x, y + 1));
+		}
+		if (right && maze[x][y + 1])
+		{
+			parents[x * cols + y + 1] = get_num(room, cols);
+			maze[x][y + 1] = false;
+			que.push_back(make_pair(x + 1, y + 2));
+		}
+		if (down && maze[x + 1][y])
+		{
+			parents[(x + 1) * cols + y] = get_num(room, cols);
+			maze[x + 1][y] = false;
+			que.push_back(make_pair(x + 2, y + 1));
+		}
+
+	}
+
+	
+	
+	
+	if (find)
+	{
+		stack<pair<int, int>> path;
+		path.push(make_pair(x_finish, y_finish));
+		
+
+		int curr_num = get_num(make_pair(x_finish, y_finish), cols);
+		while (true)
+		{
+			pair<int,int> room = get_pair(parents[curr_num], cols);
+			path.push(room);
+			if (room.first == x_start && room.second == y_start)
+				break;
+			curr_num = parents[curr_num];
+		}
+		fout << path.size() - 1 << endl;
+		while (!path.empty())
+		{
+			pair<int,int> room = path.top();
+			fout << room.first << " " << room.second << endl;
+			path.pop();
+		}
+	}
+	else
+		fout << -1;
+
+	return 0;
+}
